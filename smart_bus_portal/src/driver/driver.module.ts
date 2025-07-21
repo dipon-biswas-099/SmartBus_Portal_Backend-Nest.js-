6e5a4ba29c3ee0dfba2bc3e5114519847ee6d49d
Raihanul Islam Nahid <97145938+raihanulislam00@users.noreply.github.com>
2025-07-21 23:25:53 +0600

    main

--- smart_bus_portal/src/driver/driver.module.ts
+++ smart_bus_portal/src/driver/driver.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DriverController } from './driver.controller';
-import { DriverService } from './driver.service';
 
 @Module({
   controllers: [DriverController],
-  providers: [DriverService]
 })
 export class DriverModule {}
