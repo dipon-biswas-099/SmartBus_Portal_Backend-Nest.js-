9548aa9f600046925a1ef357e3f93909bc9123d0
dipon-biswas-099 <diponbiswas099@gmail.com>
2025-07-27 20:12:52 +0600

    file upload implemented

--- smart_bus_portal/src/driver/driver.controller.ts
+++ smart_bus_portal/src/driver/driver.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  UseInterceptors,
+  UploadedFile,
+  Res,
+  BadRequestException,
+} from '@nestjs/common';
+import { CreateDriverDto } from './create-driver.dto';
+import { DriverService } from './driver.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { MulterError } from 'multer';
+import { Response } from 'express';
 
 @Controller('driver')
-export class DriverController {}
+export class DriverController {
+  constructor(private readonly driverService: DriverService) {}
+
+  @Post('file')
+  /*@UsePipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )*/
+  @UseInterceptors(
+    FileInterceptor('nidImage', {
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|png)$/)) {
+          return cb(
+            new MulterError(
+              'LIMIT_UNEXPECTED_FILE',
+              'Only image files are allowed',
+            ),
+            false,
+          );
+        }
+        cb(null, true);
+      },
+      limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB
+      storage: diskStorage({
+        destination: '../uploads/nid',
+        filename: (req, file, cb) => {
+          const uniqueName = `${Date.now()}-${file.originalname}`;
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
+  create(
+    @Body() driverDto: CreateDriverDto,
+    @UploadedFile() nidImage: Express.Multer.File,
+  ) {
+    if (!nidImage) {
+      throw new BadRequestException(
+        'NID image is required and must be under 2MB',
+      );
+    }
+    driverDto.id = Number(driverDto.id); // Ensure id is parsed as an integer
+    // Optionally pass the image filename to the service
+    return this.driverService.createDriver({
+      ...driverDto,
+      nidImage: nidImage.filename,
+    });
+  }
+
+  @Get(':id')
+  getDriverById(@Param('id', ParseIntPipe) id: number) {
+    return this.driverService.findById(id);
+  }
+
+  @Get()
+  getAllDrivers() {
+    return this.driverService.findAll();
+  }
+
+  @Get('nid/:name')
+  getNidImage(@Param('name') name: string, @Res() res: Response) {
+    return res.sendFile(name, { root: '../uploads/nid' });
+  }
+}
